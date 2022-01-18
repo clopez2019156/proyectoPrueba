b9a704e0c48963c80628843e2fc4289902328cca
cesar <cl232326@gmail.com>
2022-01-18 16:46:48 -0600

    java

---
+++ cesarlopez/src/main/java/com/cesarlopez/CesarlopezApplication.java
@@ -0,0 +1,13 @@
+package com.cesarlopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesarlopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesarlopezApplication.class, args);
+	}
+
+}
