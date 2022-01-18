0e33a050ceba1eae799cfea67d9e169399c3daca
cesar <cl232326@gmail.com>
2022-01-18 16:43:41 -0600

    angular

--- java/cesarlopez/src/main/java/com/cesarlopez/DAO/PersonaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.cesarlopez.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cesarlopez.model.Persona;
-
-public interface PersonaDAO extends JpaRepository<Persona, Integer> {
-
-}
--- java/cesarlopez/src/main/java/com/cesarlopez/DAO/ProductoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.cesarlopez.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cesarlopez.model.Producto;
-
-public interface ProductoDAO extends JpaRepository<Producto, Integer> {
-
-}
